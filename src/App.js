fc6abfa49974cf91bfe4b9ed932083762e9d7bc4
MazvydasV <mvaicekauskas@inbox.lt>
2022-10-16 20:15:33 +0300

    feat(3) created posts and form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
+import Posts from './components/Posts/posts';
+import Form from './components/Form/form';
+
+import appground from './images/appground.jpg';
 
 const App = () => {
     return (
-        <div><h1>test</h1></div>
+        <Container maxWidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Airsoft turgelis</Typography>
+                <img src={appground} alt="background" height="60" />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container> 
     );
 };
 
