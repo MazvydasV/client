a15721654c5bc8233bb91da22179ea0de1e6a8f9
MazvydasV <mvaicekauskas@inbox.lt>
2022-10-16 20:37:44 +0300

    feat(4) created styles

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
 import Posts from './components/Posts/posts';
 import Form from './components/Form/form';
+import useStyles from './styles';
 
 import appground from './images/appground.jpg';
 
 const App = () => {
+    const classes = useStyles();
     return (
         <Container maxWidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center">Airsoft turgelis</Typography>
-                <img src={appground} alt="background" height="60" />
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Airsoft turgelis</Typography>
+                <img className={classes.image} src={appground} alt="background" height="60" />
             </AppBar>
             <Grow in>
                 <Container>
